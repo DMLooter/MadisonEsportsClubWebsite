edd47a663a88cee69de59f6e11c9f2f75542d51a
Michael Verban <verbanmike@gmail.com>
2021-11-18 22:48:49 -0600

    Added calendartable partial

---
+++ calendartable.php
@@ -0,0 +1,64 @@
+<?php
+$dbconfile = parse_ini_file("db.ini")["db_connection_file"];
+require $dbconfile;
+
+session_start();
+
+$conn = OpenConnection();
+
+$sql = "SELECT * FROM Events ORDER BY Start DESC, Title;";
+$result = mysqli_query($conn, $sql);
+
+$days = array();
+while($row = mysqli_fetch_assoc($result)) {
+	$title = $row["Title"];
+	$loc = $row["Location"];
+	$start = getdate(strtotime($row["Start"]));
+	$end = getdate(strtotime($row["End"]));
+	if(array_key_exists($start['mday'], $days) === false){
+		$days[$start["mday"]] = array();
+	}
+	$days[$start['mday']][] = $row;	
+}
+
+CloseConnection($conn);
+?>
+<table id="calendar">
+    <tbody>
+		<?php
+            $month = $_GET["month"];
+            $year = $_GET["year"];
+
+            $input = $month."-".$year;
+            $date = DateTime::createFromFormat('d-m-Y', '01-' . $input);
+            $firstdate = getdate($date->getTimestamp());
+
+			print("<tr id='header-row'><th colspan=7>".$firstdate['month']."</th></tr>");
+
+			$firstDayOfWeek = $firstdate['wday'];
+			$DIM = cal_days_in_month(0, $firstdate['mon'], $firstdate['year']);
+
+			$dow = 0;
+			for($dom = 1-$firstDayOfWeek; $dom < $DIM + (7 - ($DIM%7)); $dom++){
+				if($dow == 0){
+					print("<tr>");
+				}
+				if($dom > 0 && $dom <= $DIM){
+                    print("<td class='value'><div class='day'><div class='date'>".$dom."</div>");
+                    if(array_key_exists($dom, $days)){
+                        foreach($days[$dom] as $row){
+                            print("<div class='event'>".$row["Title"]."</div>");
+                        }
+                    }
+					print("<div class='spacer'></div></div></td>");
+				}else{
+					print("<td class='blank'></td>");
+				}
+				if($dow == 6) {
+					print("</tr>");
+				}
+				$dow = ($dow + 1) % 7;
+			}
+		?>
+    </tbody>
+</table>
