fe5aa00ea55622f1349ab1fd11b88f1419a13178
DMLooter <verbanmike@gmail.com>
2021-12-07 23:49:26 +0000

    Updated game structure

--- data/converter.py
+++ data/converter.py
@@ -15,6 +15,6 @@ with open('games.csv', newline='') as file:
 		rownum = 0;
 		for row in reader:
 			if rownum != 0:
-				output.write(f'<tr>\n<td>{row[0]}</td>\n<td>{row[1]}</td>\n</tr>\n');
+				output.write(f'<tr>\n<td><a href="/game/{row[2]}">{row[0]}</a></td>\n<td>{row[1]}</td>\n</tr>\n');
 			rownum+=1
 
---
+++ game/lol.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Michael Verban</title>
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/head.html' ?>
+	<link rel="stylesheet" type="text/css" href="/style/Home.css">
+</head>
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/header.php' ?>
+	<div id="body">
+		<h1>League of Legends</h1>
+		<h3>Competitive Info:</h3>
+		<p>We field a Varsity team that competes in Riot's official CLOL tournament along with smaller tournaments throughout the year.</p>
+		<p>We also form teams of all skill levels from iron to diamond to compete in the Style Esports League</p>
+		<p>Tryouts are held in mid-september, with those Diamond and above able to tryout for the varsity team, and all others sorted into teams based vaguely on rank below that. We don't limit to a specific number of teams each year, so there's no real pressure! The tryouts are simply for us to figure out who you play well with and what combinations of players will field the most successful of teams regardless of rank. If you miss tryouts, you can always form your own team and join our official list as well!</p>
+		<h3>Inhouses</h3>
+		<p>Inhouses are held every Sunday at 8pm. We run 5v5 Summoner's Rift customs and try to make teams as balanced as possible so that everyone has a good time without needing to separate based on skill.</p>
+<iframe src="https://discord.com/widget?id=751144062303862986&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+	</div>
+</body>
+</html>
